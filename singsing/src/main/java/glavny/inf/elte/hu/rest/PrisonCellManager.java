601dc3a77da6b81cacdf1e9f4f04ab212b7c77ea
boga95 <gabor.borsik@gmail.com>
2018-10-04 11:51:21 +0200

    Add update method for PrisonCellManager.
    SS-30

--- singsing/src/main/java/glavny/inf/elte/hu/rest/PrisonCellManager.java
+++ singsing/src/main/java/glavny/inf/elte/hu/rest/PrisonCellManager.java
@@ -1,8 +1,7 @@
 package glavny.inf.elte.hu.rest;
 
-import glavny.inf.elte.hu.data.Prisoncell;
-import glavny.inf.elte.hu.data.PrisoncellRepository;
-import glavny.inf.elte.hu.data.PrisonerRepository;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +10,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.xml.ws.Response;
-import java.util.List;
+import glavny.inf.elte.hu.data.Prisoncell;
+import glavny.inf.elte.hu.data.PrisoncellRepository;
 
 @RestController
 @RequestMapping("prisoncell")
@@ -27,23 +32,20 @@ public class PrisonCellManager {
     private PrisoncellRepository prisoncellRepository;
 
     @GetMapping("/")
-    public ResponseEntity<List<Prisoncell>> getPrisonCells(Authentication auth)
-    {
-        return new ResponseEntity<List<Prisoncell>>(prisoncellRepository.findAll(),HttpStatus.OK);
+    public ResponseEntity<List<Prisoncell>> getPrisonCells(Authentication auth) {
+        return new ResponseEntity<List<Prisoncell>>(prisoncellRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/free_space")
-    public ResponseEntity<List<Prisoncell>> getPrisonCellsWithFreeSpace(Authentication auth)
-    {
-        return new ResponseEntity<List<Prisoncell>>(prisoncellRepository.findCellWithFreeSpace(),HttpStatus.OK);
+    public ResponseEntity<List<Prisoncell>> getPrisonCellsWithFreeSpace(Authentication auth) {
+        return new ResponseEntity<List<Prisoncell>>(prisoncellRepository.findCellWithFreeSpace(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Prisoncell> getPrisonCell(@PathVariable("id") Integer id, Authentication auth)
-    {
-        Prisoncell result =  prisoncellRepository.getOne(id);
+    public ResponseEntity<Prisoncell> getPrisonCell(@PathVariable("id") Integer id, Authentication auth) {
+        Prisoncell result = prisoncellRepository.getOne(id);
         result.getPrisoners();
-        return new ResponseEntity<Prisoncell>(result,HttpStatus.OK);
+        return new ResponseEntity<Prisoncell>(result, HttpStatus.OK);
     }
 
     @PostMapping("/new")
@@ -54,14 +56,22 @@ public class PrisonCellManager {
             return new ResponseEntity<Void>(HttpStatus.CONFLICT);
         }
         HttpHeaders headers = new HttpHeaders();
-        //headers.setLocation(builder.path("/{id}").buildAndExpand(c.getId()).toUri());
+        // headers.setLocation(builder.path("/{id}").buildAndExpand(c.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Void> updatePrisonCell(@RequestBody Prisoncell c, UriComponentsBuilder builder) {
+        prisoncellRepository.save(c);
+
+        HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
 
     @PostMapping("/delete")
     public ResponseEntity<Void> deletePrisonCell(@RequestBody Prisoncell c, UriComponentsBuilder builder) {
         boolean flag = true;
-        if(c.getPrisoners().size() > 0 ) {
+        if (c.getPrisoners().size() > 0) {
             return new ResponseEntity<Void>(HttpStatus.CONFLICT);
         }
         prisoncellRepository.delete(c);
